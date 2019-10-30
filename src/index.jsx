ec93ffb2b88e7a74b4616437319cf5cebc7ba7ab
Sergey Stadnik <sergey.stadnik@marketplacer.com>
2019-10-30 11:20:28 +1100

    Disable NavMenu,style  and globalStyle

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as style from "./style";
-import NavMenu from "./NavMenu";
+// import * as style from "./style";
+// import NavMenu from "./NavMenu";
 import Sect1 from "./Sect1";
 import Sect2 from "./Sect2";
 import Sect3 from "./Sect3";
@@ -32,18 +32,18 @@ class Index extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <style.globalStyle />
-        <button onClick={this.scrollLinkA}>Div1</button>
-        <button onClick={this.scrollLinkB}>Div2</button>
-        <NavMenu onClick={this.scrollLink} />
-        <div ref={this.scrollRef1}>
-          <Sect1 />
-        </div>
-        <div ref={this.scrollRef2}>
-          <Sect2 />
-        </div>
-        <Sect3 />
-      </React.Fragment>
+         {/* <style.globalStyle /> */}
+         <button onClick={this.scrollLinkA}>Div1</button>
+         <button onClick={this.scrollLinkB}>Div2</button>
+         {/* <NavMenu onClick={this.scrollLink} /> */}
+         <div ref={this.scrollRef1}>
+           <Sect1 />
+         </div>
+         <div ref={this.scrollRef2}>
+           <Sect2 />
+         </div>
+         <Sect3 />
+       </React.Fragment>
     );
   }
 }
